02454afdec1356aa5ccab7d68f64057ad128b1f2
Emma Slager <emmajean.slager@gmail.com>
2020-02-12 13:58:10 -0800

    change files

--- README.md
+++
@@ -1,4 +0,0 @@
-# examplerepo
-this is a demo for TGIS 503
-
-Adding a note at 5:59pm on Tuesday to demonstrate GitHub in class.
---
+++ scripts.js
@@ -0,0 +1,47 @@
+var map = L.map('map').setView([47.25, -122.44], 11);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/light-v10',
+    accessToken: 'pk.eyJ1IjoiZWpzbGdyIiwiYSI6ImNrMnBtbDF0YzA1d2UzbnJtbHMxcWltOTIifQ.MY_BZXu-EPtwTlxsI_zgcQ',
+}).addTo(map);
+
+var control = L.Routing.control({
+    waypoints: [],
+    routeWhileDragging: true,
+    units:'imperial',
+    router: L.Routing.mapbox('pk.eyJ1IjoiZWpzbGdyIiwiYSI6ImNrMnBtbDF0YzA1d2UzbnJtbHMxcWltOTIifQ.MY_BZXu-EPtwTlxsI_zgcQ'),
+    geocoder: L.Control.Geocoder.mapbox('pk.eyJ1IjoiZWpzbGdyIiwiYSI6ImNrMnBtbDF0YzA1d2UzbnJtbHMxcWltOTIifQ.MY_BZXu-EPtwTlxsI_zgcQ'),
+    collapsible: true,
+    show: false,
+}).addTo(map);
+
+function createButton(label, container) {
+    var btn = L.DomUtil.create('button', '', container);
+    btn.setAttribute('type', 'button');
+    btn.innerHTML = label;
+    return btn;
+}
+
+map.on('click', function(e) {
+    var container = L.DomUtil.create('div'),
+        startBtn = createButton('Start from this location', container),
+        destBtn = createButton('Go to this location', container);
+
+    L.popup()
+        .setContent(container)
+        .setLatLng(e.latlng)
+        .openOn(map);
+
+    L.DomEvent.on(startBtn, 'click', function() {
+        control.spliceWaypoints(0, 1, e.latlng);
+        map.closePopup();
+    });
+
+    L.DomEvent.on(destBtn, 'click', function() {
+        control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
+        control.show();
+        map.closePopup();
+    });
+});
